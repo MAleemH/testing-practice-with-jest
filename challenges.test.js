bdea95d9b1d23c27b9436bed0d7d3753d05b8988
Your Name <you@example.com>
2023-01-25 15:05:56 +0500

    reverse string function added

--- challenges.test.js
+++ challenges.test.js
@@ -1,5 +1,23 @@
-const sum = require('./challenges');
+// string length
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+const stringLength = require('./stringLength');
+
+test('Returns number of strings if string is between 1-10', () => {
+  expect(stringLength("abc")).toBe(3);
+});
+
+test('Throw error if string length is less than 1', () => {
+  expect(() => stringLength("")).toThrow(Error);
+});
+
+test('Throw error if string length is less than 1', () => {
+  expect(() => stringLength("abcdefghijklmno")).toThrow(Error);
+});
+
+// reverse string
+
+const reverseString = require('./reverseString');
+
+test('Reverse a string', () => {
+  expect(reverseString('hello')).toBe('olleh');
 });
